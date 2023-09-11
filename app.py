650c6ceb4cd810eaf6ee5316726b3f560a7d36e1
Lucas Duarte Batista <92119989+LuckasDuarte@users.noreply.github.com>
2023-09-11 18:38:15 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# ---------- Gerador de Senhas Com python ----------
+# Desenvolvido por: Lucas Duarte
+
+# ---------- Modulos ----------
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+#PIL 
+from PIL import ImageTk, Image 
+
+import random
+import string
+
+
+
+
+
+
